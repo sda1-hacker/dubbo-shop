1de96abd408b79a9f2cda44549e18d1039be51c7
sda1_hacker <760557109@qq.com>
2020-09-21 23:31:19 +0800

    rabbitmq 交换机

---
+++ sda1-mq/rabbitqm-java/src/main/java/com/dubbo/shop/routing/Recv1.java
@@ -0,0 +1,50 @@
+package com.dubbo.shop.routing;
+
+import com.rabbitmq.client.*;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+public class Recv1 {
+
+    private static final String QUEUE_NAME = "routing_exchange1";
+
+    private static final String EXCHANGE_NAME = "direct_exchange";
+
+    public static void main(String[] args) throws IOException, TimeoutException {
+
+        ConnectionFactory cf = new ConnectionFactory();
+        cf.setVirtualHost("sda1_mq");
+        cf.setUsername("sda1");
+        cf.setPassword("123456");
+        cf.setHost("192.168.157.128");
+        cf.setPort(5672);
+
+        Connection connection = cf.newConnection();
+
+        // 通过channel进行消息的传递
+        Channel channel = connection.createChannel();
+
+        // 声明队列
+        channel.queueDeclare(QUEUE_NAME, true, false, false, null);
+
+        // 将队列和交换机进行绑定
+        // 队列名， 交换机名， 路由规则(路由key)
+        channel.queueBind(QUEUE_NAME, EXCHANGE_NAME, "nba");
+
+        // 声明一个消费者
+        Consumer consumer = new DefaultConsumer(channel){
+            @Override
+            public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException {
+                // 在这里处理接受到的信息
+                String msg = new String(body, "utf-8");
+                System.out.println("消费者1  接受到的消息为：" + msg);
+            }
+        };
+
+        // 让消费者去监听队列
+        // 队列名称，自动回复（true），消费者
+        channel.basicConsume(QUEUE_NAME, true, consumer);
+
+    }
+}
---
+++ sda1-mq/rabbitqm-java/src/main/java/com/dubbo/shop/topic/Sender.java
@@ -0,0 +1,63 @@
+package com.dubbo.shop.topic;
+
+// rabbitmq 发布订阅模式
+
+// 发布者 -- 交换机  -- 队列 -- 消费者
+// 发布者和交换机相连，因此在这里不需要声明队列    消费者和队列相连，
+// 交换机的消息会传递到每一个队列里面。
+// 交换机并不存储消息，只是做消息的转发，如果没有队列则清空对应的消息。
+
+// topic类型的交换机 -- 满足条件（通配符）的消息会转发到不通的队列中
+// 和 direct（精确匹配） 类似， 只是topic是模糊匹配
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+// 生产者
+public class Sender {
+
+    private static final String EXCHANGE_NAME = "topic_exchange";
+
+    public static void main(String[] args) throws IOException, TimeoutException {
+        // 1. 连接到rabbitmq服务器
+        ConnectionFactory cf = new ConnectionFactory();
+        // virtual host
+        cf.setVirtualHost("sda1_mq");   // 这个路径要和rabbitmq控制台中的路径保持一直，/不能省略
+        // 用户名
+        cf.setUsername("sda1");
+        // 密码
+        cf.setPassword("123456");
+        // 服务器ip
+        cf.setHost("192.168.157.128");
+        // 服务端口
+        cf.setPort(5672);
+
+        // 2. 创建队列
+        Connection connection = cf.newConnection();
+        // 通过channel进行通信
+        Channel channel = connection.createChannel();
+
+        // 创建交换机 -- 类型是  topic  根据 routing key进行路由转发
+        channel.exchangeDeclare(EXCHANGE_NAME, "topic");
+
+        // 3. 发送消息
+
+        String msg1 = "NBA...";
+        String msg2 = "CBA...";
+        // 交换机名字， 路由key，默认规则，消息
+        for(int i = 1; i < 10; i++){
+            channel.basicPublish(EXCHANGE_NAME, "rabbit_mq_first_topic", null, (msg1+i).getBytes());
+            channel.basicPublish(EXCHANGE_NAME, "rabbit_qm_second_topic", null, (msg2+i).getBytes());
+
+        }
+
+        System.out.println("发送成功");
+        System.exit(0);
+
+    }
+
+}
